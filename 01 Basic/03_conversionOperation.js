6fb380b3cae32d85868f8b51ab1e3bfcd872630a
Iman-Datta <dattaiman56@gmail.com>
2025-07-06 01:07:48 +0530

    Oparation

--- 01 Basic/03_conversionOperation.js
+++ 01 Basic/03_conversionOperation.js
@@ -1,21 +1,19 @@
-let score = "33" // string
-console.log (typeof score)
-let valueInNumber = Number(score)
-console.log(valueInNumber)
+let score = "33"; // string
+console.log(typeof score);
+let valueInNumber = Number(score);
+console.log(valueInNumber);
 
-
-let string_score = "33abc"
-let num_score = Number(string_score)
+let string_score = "33abc";
+let num_score = Number(string_score);
 console.log(num_score);
 
+let name = null;
+let num_name = Number(name);
+console.log(num_name);
 
-let name = null
-let num_name = Number(name)
-console.log = (num_name)
-
-let email = undefined
-let num_email = Number(name)
-console.log = (num_email)
+let email = undefined;
+let num_email = Number(email);
+console.log(num_email);
 
 // "33" => 33
 // "33abc" => NaN (Not in Number) but it is a Number data type
@@ -25,3 +23,40 @@ console.log = (num_email)
 
 // 1 => true; 0 => false
 // "" => false; "Iman" => true
+
+// ********************************** Operations *******************************************************
+
+let value = 3;
+let ng_value = -value;
+console.log(ng_value);
+
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 / 2);
+console.log(2 ** 2);
+console.log(3 % 2);
+
+let str1 = "Hello";
+let str2 = "Iman";
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(3 + ((4 * 5) % 3));
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
