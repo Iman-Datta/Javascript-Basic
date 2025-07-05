7472667097650a6ea166e61b19ba8bb31cc99228
Iman-Datta <dattaiman56@gmail.com>
2025-07-06 00:09:34 +0530

    Initial commit

---
+++ 01 Basic/01_variable.js
@@ -0,0 +1,20 @@
+const accountId = 144553;
+let accountEmail = "example@gmail.com";
+var accountPassword = "12345";
+accountCity = "Kolkata";
+let accountState;
+
+console.table([accountId, accountEmail,accountPassword,accountCity])
+// accountId = 2 // Not allowed: TypeError: Assignment to constant variable.
+
+accountEmail = "hc@hc.com";
+accountPassword = "212121"
+accountCity = "Bardhaman"
+
+console.log("New data")
+console.table([accountId, accountEmail,accountPassword,accountCity, accountState])
+
+/*
+Prefer not to use var
+because of issue in block scope and funtional scope
+*/
