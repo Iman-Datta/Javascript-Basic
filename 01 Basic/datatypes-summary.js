fc66f10f9e8889080c24e147b6bb4013f7a58d88
Iman-Datta <dattaiman56@gmail.com>
2025-07-06 14:39:38 +0530

    Memory

--- 01 Basic/datatypes-summary.js
+++ 01 Basic/datatypes-summary.js
@@ -2,37 +2,67 @@
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-const score = 100
-const scoreValue = 100.3
+const score = 100;
+const scoreValue = 100.3;
 
-const isLoggedIn = false
-const outsideTemp = null
+const isLoggedIn = false;
+const outsideTemp = null;
 let userEmail;
 
 // let score: Number = 10 it show error
-const id = Symbol('123') // unique
-const anotherId = Symbol('123')
+const id = Symbol("123"); // unique
+const anotherId = Symbol("123");
 
 console.log(id === anotherId);
 
-// const bigNumber = 3456543576654356754n
-
-
+const bigNumber = 3456543576654356754n; //BigInt data type
 
 // Reference (Non primitive)
 
 // Array, Objects, Functions
 
-const heros = ["shaktiman", "naagraj", "doga"];
-let myObj = {
-    name: "hitesh",
-    age: 22,
-}
+const heros = ["shaktiman", "naagraj", "doga"]; // Array
 
-const myFunction = function(){
-    console.log("Hello world");
-}
+let myObj = {
+  // Object => kye: value
+  name: "hitesh",
+  age: 22,
+};
+
+const myFunction = function () {
+  // function is store as a variable
+  console.log("Hello world");
+};
 
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack => Primitive; Heap => Non-Primitive
+
+let myEmaillName = "imandatta@gmail.com" // myEmaillName this store in stack memory and "imandatta" store in heap memory
+
+let myYoutubeName = "imandattayoutube.com"
+let anotherName = myEmaillName
+anotherName = "codewithiman"
+
+console.log(myEmaillName)
+console.log(anotherName)
+
+let userOne = {
+  email : "user@gmail.com",
+  upi : "user@ybl",
+}
+
+let userTwo = userOne
+
+userTwo.email = "iman@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
